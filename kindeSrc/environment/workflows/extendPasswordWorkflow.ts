f27e5f213d07d58b8c16405a790ce0e298c95222
Brett Chaldecott <brett@rapidhealth.co.uk>
2025-01-15 12:40:19 +0200

    update: added password verification

---
+++ kindeSrc/environment/workflows/extendPasswordWorkflow.ts
@@ -0,0 +1,18 @@
+import { onUserTokenGeneratedEvent,version, createKindeAPI, getEnvironmentVariable, accessTokenCustomClaims, WorkflowSettings, WorkflowTrigger, denyAccess, fetch } from "@kinde/infrastructure"
+import { settings } from "../../../utils/utils.js";
+
+export const workflowSettings = {
+  id: "externalPasswordValidation",
+  name: "Reset Password External Validation",
+  trigger: "user:new_password_provided",
+  bindings: {
+    "kinde.secureFetch": {},
+    "kinde.widget": {}
+  }
+};
+
+export default {
+  async handle(event: any) {
+    console.log("Hello to the world");
+  }
+}
